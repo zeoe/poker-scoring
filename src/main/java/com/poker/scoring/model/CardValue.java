ee261b80db52a290d703ad9f945630b3d788f134
ZerdusÖzkan <zerdus.oezkan@rio.cloud>
2023-11-18 13:36:25 +0100

    Add score to CardValue

--- src/main/java/com/poker/scoring/model/CardValue.java
+++ src/main/java/com/poker/scoring/model/CardValue.java
@@ -2,17 +2,27 @@
 package com.poker.scoring.model;
 
 public enum CardValue {
-  TWO,
-  THREE,
-  FOUR,
-  FIVE,
-  SIX,
-  SEVEN,
-  EIGHT,
-  NINE,
-  TEN,
-  JACK,
-  QUEEN,
-  KING,
-  ACE
+  TWO(2),
+  THREE(3),
+  FOUR(4),
+  FIVE(5),
+  SIX(6),
+  SEVEN(7),
+  EIGHT(8),
+  NINE(9),
+  TEN(10),
+  JACK(11),
+  QUEEN(11),
+  KING(12),
+  ACE(13);
+
+  private final int score;
+
+  CardValue(int score) {
+    this.score = score;
+  }
+
+  public int getScore() {
+    return score;
+  }
 }
